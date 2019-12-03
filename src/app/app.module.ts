916df4a563ef5dc6705c67f0ec73c0479eb56adf
Autri Baghkhanian <autri.baghkhanian@gmail.com>
2019-12-02 23:44:45 -0800

    big push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './people-app.component';
+import { PeopleListComponent } from './people/people-list/people-list.component';
+import { PeopleThumbnailComponent } from './people/people-thumbnail/people-thumbnail.component';
+import { PeopleDetailsComponent } from './people/people-details/people-details.component';
+import { RouterModule } from '@angular/router';
+import { PeopleService } from './people/people.service';
+import { appRoutes } from './route';
+import { NavbarComponent } from './nav/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    PeopleListComponent,
+    PeopleThumbnailComponent,
+    PeopleDetailsComponent,
+    NavbarComponent
+  ],
+  providers: [
+    PeopleService,
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
